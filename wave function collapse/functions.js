c050b15dccff69e976d6a453e58f69c1c321f7ad
instaprofile <69371466+meguminrules@users.noreply.github.com>
2022-04-24 20:36:38 -0500

    Add files via upload

--- wave function collapse/functions.js
+++ wave function collapse/functions.js
@@ -12,17 +12,46 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const getCommonElements = (arrays) => {//Assumes that we are dealing with an array of arrays of integers
+    var currentValues = {};
+    var commonValues = {};
+    for (var i = arrays[0].length - 1; i >= 0; i--) {//Iterating backwards for efficiency
+        currentValues[arrays[0][i]] = 1; //Doesn't really matter what we set it to
+    }
+    for (var i = arrays.length - 1; i > 0; i--) {
+        var currentArray = arrays[i];
+        for (var j = currentArray.length - 1; j >= 0; j--) {
+            if (currentArray[j] in currentValues) {
+                commonValues[currentArray[j]] = 1; //Once again, the `1` doesn't matter
+            }
+        }
+        currentValues = commonValues;
+        commonValues = {};
+    }
+    return Object.keys(currentValues).map(function (value) {
+        return value;
+    });
+}
+
+const getRandomTile = (x, y, tileArray) => {
+    // LOOKING FOR WHAT ADJACENT TILES CAN TOUCH AND SEEING IF THEY HAVE A COMMON THING IF IT'S ONE OR MORE THEN RANDOMIZE IT
+    let aTiles = getAdjacentTiles(x, y, tileArray).filter(x => x != null && x.type != "air");
+
+    if (aTiles.length <= 0) return tileArray[x][y];
+
+    let rules = Array.from(aTiles, x => RULES[x.type]);
+    let commonElements = getCommonElements(rules);
+
+    return (commonElements.length != 0) ? new Tile(x, y, TILESIZE, TILESIZE, tiles[commonElements[getRandomInt(0, commonElements.length - 1)]]) : []
+}
+
 const getAdjacentTiles = (x, y, tileArray) => {
-    let topLeft = tileArray[x - 1][y - 1]
-    let top = tileArray[x][y - 1]
-    let topRight = tileArray[x + 1][y - 1]
+    let top = (y - 1 < 0) ? null : tileArray[x][y - 1]
 
-    let midLeft = tileArray[x - 1][y]
-    let midRight = tileArray[x + 1][y]
+    let midLeft = (x - 1 < 0) ? null : tileArray[x - 1][y]
+    let midRight = (x + 1 > WIDTH / TILESIZE - 1) ? null : tileArray[x + 1][y]
 
-    let botLeft = tileArray[x - 1][y + 1]
-    let bot = tileArray[x][y + 1]
-    let botRight = tileArray[x + 1][y + 1]
+    let bot = (y + 1 > HEIGHT / TILESIZE - 1) ? null : tileArray[x][y + 1]
 
-    return [topLeft, top, topRight, midLeft, midRight, botLeft, bot, botRight]
+    return [top, midLeft, midRight, bot]
 }
--- wave function collapse/index.js
+++ wave function collapse/index.js
@@ -1,9 +1,9 @@
 const WIDTH = 1800;
 const HEIGHT = 900;
-const TILESIZE = 75;
+const TILESIZE = 30;
 
 class Sprite {
-    constructor (imgPath, width, height) {
+    constructor(imgPath, width, height) {
         let img = document.createElement("img");
         img.setAttribute("src", imgPath);
         this.img = img;
@@ -12,32 +12,69 @@ class Sprite {
     }
 }
 
-const tiles = {"grass": new Sprite("/tiles/grass.png", TILESIZE, TILESIZE), 
-               "sand": new Sprite("/tiles/sand.png", TILESIZE, TILESIZE), 
-               "water": new Sprite("/tiles/water.png", TILESIZE, TILESIZE),
-                "tree": new Sprite("/tiles/tree-grass.png", TILESIZE, TILESIZE)};
+const tiles = {
+    "grass": new Sprite("/tiles/grass.png", TILESIZE, TILESIZE),
+    "sand": new Sprite("/tiles/sand.png", TILESIZE, TILESIZE),
+    "cactus": new Sprite("/tiles/cactus.png", TILESIZE, TILESIZE),
+    "water": new Sprite("/tiles/water.png", TILESIZE, TILESIZE),
+    "tree": new Sprite("/tiles/tree.png", TILESIZE, TILESIZE),
+    "air": new Sprite("/tiles/air.png", TILESIZE, TILESIZE)
+};
+
+const RULES = {
+    "tree": [
+        "grass",
+        "tree"
+    ],
+    "grass": [
+        "grass",
+        "tree",
+        "sand"
+    ],
+    "sand": [
+        "sand",
+        "water",
+        "grass",
+        "cactus"
+    ],
+    "cactus": [
+        "cactus",
+        "sand",
+    ],
+    "water": [
+        "sand",
+        "water"
+    ]
+}
 
 class Tile {
-    constructor (x, y, W, H, sprite) {
+    constructor(x, y, W, H, sprite) {
         this.x = x
         this.y = y
         this.WIDTH = W;
         this.HEIGHT = H;
         this.sprite = sprite;
+        this.type = sprite.img.getAttribute("src").split("/")[2].split(".")[0]
     }
-    
-    drawTile (ctx) {
+
+    updateSprite(sprite) {
+        this.sprite = sprite;
+        this.type = sprite.img.getAttribute("src").split("/")[2].split(".")[0];
+    }
+
+    drawTile(ctx) {
         // ctx.beginPath();
         // ctx.strokeRect(this.x, this.y, this.WIDTH, this.HEIGHT)
         // ctx.fillStyle = this.color;
         // ctx.fillRect(this.x, this.y, this.WIDTH, this.HEIGHT)
         // ctx.stroke();
+        this.type = this.sprite.img.getAttribute("src").split("/")[2].split(".")[0]
         ctx.drawImage(this.sprite.img, this.x, this.y, this.sprite.WIDTH, this.sprite.HEIGHT);
     }
 }
 
 class Stage {
-    constructor () {
+    constructor() {
         this.TILESIZE = TILESIZE;
         this.map = [];
 
@@ -45,13 +82,13 @@ class Stage {
         for (let i = 0; i < WIDTH / this.TILESIZE; i++) {
             this.map.push([]);
             for (let j = 0; j < HEIGHT / this.TILESIZE; j++) {
-                this.map[i].push(new Tile(i * this.TILESIZE, j * this.TILESIZE, this.TILESIZE, this.TILESIZE, tiles["tree"]));
+                this.map[i].push(new Tile(i * this.TILESIZE, j * this.TILESIZE, this.TILESIZE, this.TILESIZE, tiles["air"]));
             }
         }
     }
 
     // Draw function for drawing the tiles
-    draw (ctx) {
+    draw(ctx) {
         this.map.forEach(x => {
             x.forEach(y => {
                 y.drawTile(ctx)
@@ -61,7 +98,7 @@ class Stage {
 }
 
 class App {
-    constructor (W, H, bgColor) {
+    constructor(W, H, bgColor) {
         this.WIDTH = W;
         this.HEIGHT = H;
         this.bgColor = bgColor;
@@ -90,41 +127,35 @@ document.querySelector("canvas").addEventListener("mousedown", (evt) => {
     // Code Galore... this finds what tile the mouse is over and toggle it on and off
     let [x, y] = [Math.round((pos.x - app.stage.TILESIZE / 2) / app.stage.TILESIZE), Math.round((pos.y - app.stage.TILESIZE / 2) / app.stage.TILESIZE)];
     let clickedTileSprite = app.stage.map[x][y].sprite;
-
-    console.log(clickedTileSprite)
-
-    if (evt.button == 0) {
-        switch (clickedTileSprite) {
-            case tiles["tree"]:
-                app.stage.map[x][y].sprite = tiles["grass"];
-                break;
-            case tiles["grass"]:
-                app.stage.map[x][y].sprite = tiles["sand"];
-                break;
-            case tiles["sand"]:
-                app.stage.map[x][y].sprite = tiles["water"];
-                break;
-            case tiles["water"]:
-                app.stage.map[x][y].sprite = tiles["tree"];
-                break;
-        }
-    } else if (evt.button == 2) {
-        switch (clickedTileSprite) {
-            case tiles["tree"]:
-                app.stage.map[x][y].sprite = tiles["water"];
-                break;
-            case tiles["grass"]:
-                app.stage.map[x][y].sprite = tiles["tree"];
-                break;
-            case tiles["sand"]:
-                app.stage.map[x][y].sprite = tiles["grass"];
-                break;
-            case tiles["water"]:
-                app.stage.map[x][y].sprite = tiles["sand"];
-                break;
-        }
-    }
 });
 
-// Updates Canvas
-setInterval(() => { app.stage.draw(app.ctx) }, 25)
+window.addEventListener("DOMContentLoaded", () => {
+    let [X, Y] = [8, 4];
+
+    app.stage.map[X][Y].updateSprite(tiles["grass"]);
+
+    let checkedTiles = [app.stage.map[X][Y]]
+
+    const recursion = (x, y, checkedTiles) => {
+        getAdjacentTiles(x, y, app.stage.map).filter(x => x != null).forEach(x => {
+            if (checkedTiles.includes(x)) return;
+            try {
+                var randomTile = getRandomTile(x.x / TILESIZE, x.y / TILESIZE, app.stage.map).sprite;
+                x.updateSprite(randomTile);
+                checkedTiles.push(x)
+                if (checkedTiles.length >= app.stage.map.flat().length) {
+                    return
+                } else {
+                    recursion(x.x / TILESIZE, x.y / TILESIZE, checkedTiles)
+                }
+            } catch (error) {
+                checkedTiles = [app.stage.map[X][Y]]
+                recursion(X, Y, checkedTiles);
+            }
+        });
+    }
+
+    recursion(X, Y, checkedTiles)
+});
+
+setInterval(() => { app.stage.draw(app.ctx) }, 50);
